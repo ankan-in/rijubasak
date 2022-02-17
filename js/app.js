5019cfd871755f5709460763d74a626b2371ace3
Ankan <akmdgamer1290@gmail.com>
2022-02-17 13:50:00 +0530

    MAA KI AANKAN

--- js/app.js
+++ js/app.js
@@ -1,9 +1,26 @@
+//warn
+console.log(
+    "%cWait!",
+    "color: rgb(255, 218, 56); font-size: 50px; -webkit-text-stroke: black 1px;"
+  );
+  console.log(
+    "%cPasting here anything might ruin your experience in this website!",
+    "color: rgb(255, 218, 56); font-size: 20px; -webkit-text-stroke: black 1px;"
+  );
+
+
+
+
+
 const texts = ['Student' , 'Developer' , 'Programmer' , 'Hacker'];
+
 let count = 0;
 let index = 0;
 let currenText = '';
 
 let letter = '';
+
+
 
 (function type() {
 
@@ -18,5 +35,6 @@ let letter = '';
         count++;
         index = 0;
     }
+    
     setTimeout(type, 200);
 }());
